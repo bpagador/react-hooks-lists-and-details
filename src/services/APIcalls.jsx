15aa814843235e194129b052e6e9f31adaaeefe7
Briseida Pagador <bpagador@gmail.com>
2020-07-27 15:48:13 -0700

    skeleton / first trial service calls

---
+++ src/services/APIcalls.jsx
@@ -0,0 +1,18 @@
+export const getSingleCharacter = name => {
+  return fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${name}`)
+    .then(res => res.json())
+    .then(character => ({
+      name: character.name,
+      image: character.image
+    }));
+};
+
+export const getCharacters = () => {
+  return fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
+    .then(res => res.json())
+    .then(characters => characters.map(character => ({
+      name: character.name,
+      image: character.image
+    })));
+
+};
