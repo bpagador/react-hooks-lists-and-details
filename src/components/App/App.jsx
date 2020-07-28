cba5edb4e5c4b2429bd7fecbbef0ca55d160a92f
Briseida Pagador <bpagador@gmail.com>
2020-07-28 14:11:38 -0700

    added header that works and displays

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,10 +7,12 @@ import {
 
 import DisplayCharacters from '../../containers/DisplayCharacters';
 import RenderDetails from '../../containers/RenderDetails';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={DisplayCharacters} />
         <Route path="/:name" component={RenderDetails} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <Link to="/">
+      <h3>Hey Arnold! Character List</h3>
+    </Link>
+  </header>
+);
+
+export default Header;
